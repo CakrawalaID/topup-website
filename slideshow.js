6fce15843aba718681280b7fa13b432e86138a6e
CakrawalaID <abiramahaidar@gmail.com>
2026-02-01 19:50:24 +0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -6,3 +6,11 @@ setInterval(() => {
   current = (current + 1) % slides.length;
   slides[current].classList.add('active');
 }, 5000); // ganti slide tiap 5 detik
+const slides = document.querySelectorAll('.banner-slideshow img');
+let current = 0;
+
+setInterval(() => {
+  slides[current].classList.remove('active');
+  current = (current + 1) % slides.length;
+  slides[current].classList.add('active');
+}, 3000);
